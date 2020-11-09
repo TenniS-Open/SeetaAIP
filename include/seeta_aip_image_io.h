5b85bece3ab25ffb66949593f3968ded3efcbede
kaizhou.li <kaizhou.li@seetatech.com>
2020-11-09 16:45:58 +0800

    using static stb image

--- include/seeta_aip_image_io.h
+++ include/seeta_aip_image_io.h
@@ -8,8 +8,10 @@
 #include "seeta_aip_struct.h"
 #include "seeta_aip_image.h"
 
+#define STB_IMAGE_STATIC
 #define STB_IMAGE_IMPLEMENTATION
 #include "./stb_image.h"
+#define STB_IMAGE_WRITE_STATIC
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "./stb_image_write.h"
 
