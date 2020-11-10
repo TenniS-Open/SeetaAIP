34f66fc6e6d15d76c8b7c62fa3a088f8a6598273
kaizhou.li <kaizhou.li@seetatech.com>
2020-11-10 10:12:12 +0800

    update imwrite using c++ string api

--- include/seeta_aip_image_io.h
+++ include/seeta_aip_image_io.h
@@ -20,59 +20,56 @@
 
 namespace seeta {
     namespace aip {
-            static seeta::aip::ImageData imread(const std::string &filename) {
-                int iw, ih, n;
-                iw = ih = n = 0;
-                unsigned char *data = stbi_load(filename.c_str(), &iw, &ih, &n, 3);
-                if (!data) return seeta::aip::ImageData();
-                seeta::aip::ImageData result(SEETA_AIP_FORMAT_U8RGB, iw, ih, n, data);
-                stbi_image_free(data);
-                return result;
+        static seeta::aip::ImageData imread(const std::string &filename) {
+            int iw, ih, n;
+            iw = ih = n = 0;
+            unsigned char *data = stbi_load(filename.c_str(), &iw, &ih, &n, 3);
+            if (!data) return seeta::aip::ImageData();
+            seeta::aip::ImageData result(SEETA_AIP_FORMAT_U8RGB, iw, ih, n, data);
+            stbi_image_free(data);
+            return result;
+        }
+
+        static bool imwrite(const std::string &filename, const SeetaAIPImageData &image) {
+            auto image_t = image;
+            seeta::aip::ImageData tmp;
+            if (image_t.format != SEETA_AIP_FORMAT_U8RGB && image_t.format != SEETA_AIP_FORMAT_U8RGBA) {
+                tmp = convert(1, SEETA_AIP_FORMAT_U8RGB, image_t);
+                image_t = tmp;
+            }
+            auto dot_pos = filename.rfind('.');
+            std::string ext = dot_pos == std::string::npos ? "" : filename.substr(dot_pos + 1);
+            for (auto &ch : ext) ch = std::tolower(ch);
+
+            if (ext.empty()) {
+                std::cerr << "Unknown image type, image not of any known type, or corrupt" << std::endl;
+                return false;
             }
 
-            static int imwrite(const std::string &filename, const SeetaAIPImageData &image) {
-                auto rgb_image_t = convert(1, SEETA_AIP_FORMAT_U8RGB, image);
-                const SeetaAIPImageData &rgb_image = rgb_image_t;
-                auto c_filename = filename.c_str();
-                const char *ext = strrchr(c_filename, '.');
-                if (!ext)
-                {
-                    return stbi__err("unknown image type", "image not of any known type, or corrupt");
-                }
-
-                if ((strcasecmp(ext, ".jpeg") == 0) || (strcasecmp(ext, ".jpg") == 0))
-                {
-                    return  stbi_write_jpg(c_filename,
-                                           rgb_image.width, rgb_image.height, rgb_image.channels, rgb_image.data,
-                                           90);
-                }
-                else if (strcasecmp(ext, ".png") == 0)
-                {
-                    return stbi_write_png(c_filename,
-                                          rgb_image.width, rgb_image.height, rgb_image.channels, rgb_image.data,
-                                          0);
-                }
-                else if (strcasecmp(ext, ".tga") == 0)
-                {
-                    return  stbi_write_tga(c_filename,
-                                           rgb_image.width, rgb_image.height, rgb_image.channels, rgb_image.data);
-                }
-                else if (strcasecmp(ext, ".hdr") == 0)
-                {
-                    return  stbi_write_hdr(c_filename,
-                                           rgb_image.width, rgb_image.height, rgb_image.channels,
-                                           (float *)rgb_image.data);;
-                }
-                else if (strcasecmp(ext, ".bmp") == 0)
-                {
-                    return  stbi_write_bmp(c_filename,
-                                           rgb_image.width, rgb_image.height, rgb_image.channels,
-                                           rgb_image.data);;
-                }
-
-                return stbi__err("unknown image type", "Image not of any known type, or corrupt");
-
+            if (ext == "jpeg" || ext == "jpg") {
+                return stbi_write_jpg(filename.c_str(),
+                                      image_t.width, image_t.height, image_t.channels, image_t.data,
+                                      90);
+            } else if (ext == "png") {
+                return stbi_write_png(filename.c_str(),
+                                      image_t.width, image_t.height, image_t.channels, image_t.data,
+                                      0);
+            } else if (ext == "bmp") {
+                return stbi_write_bmp(filename.c_str(),
+                                      image_t.width, image_t.height, image_t.channels,
+                                      image_t.data);;
+            } else if (ext == "tga") {
+                return stbi_write_tga(filename.c_str(),
+                                      image_t.width, image_t.height, image_t.channels, image_t.data);
+            } else if (ext == "hdr") {
+                return stbi_write_hdr(filename.c_str(),
+                                      image_t.width, image_t.height, image_t.channels,
+                                      (float *) image_t.data);;
             }
+
+            std::cerr << "Unknown image type, image not of any known type, or corrupt" << std::endl;
+            return false;
+        }
     }
 }
 
